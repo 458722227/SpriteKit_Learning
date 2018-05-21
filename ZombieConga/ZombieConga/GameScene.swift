30569733be023ef874d62ba801074c5c31cf008b
4wd-ios <4wd-ios@4wd-iosdeiMac.local>
2018-05-21 17:53:59 +0800

    ZombieConga init

---
+++ ZombieConga/ZombieConga/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  ZombieConga
+//
+//  Created by 4wd-ios on 2018/5/21.
+//  Copyright © 2018年 ty4wd. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor.black
+    }
+}
---
+++ ZombieConga/ZombieConga/GameViewController.swift
@@ -0,0 +1,47 @@
+//
+//  GameViewController.swift
+//  ZombieConga
+//
+//  Created by 4wd-ios on 2018/5/21.
+//  Copyright © 2018年 ty4wd. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let scene = GameScene(size: self.view.bounds.size)
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
